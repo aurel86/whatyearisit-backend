b4770375a77c6e0a73bd09458a8a6fe9489d40de
Aurelien <a.sanctorum1301@gmail.com>
2024-06-28 12:51:00 +0200

    whatyearisit-backend

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/year', (req, res) => {
   const date = new Date();
-  res.json({ year: date.getFullYear() })
+  const yearString = date.getFullYear().toString();
+  res.json({ year: yearString() })
 });
 
 
